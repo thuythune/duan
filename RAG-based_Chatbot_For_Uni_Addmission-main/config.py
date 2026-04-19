b10aa539d4c9abb4a5e1e088e95cddd395906352
thuythune <thut06920@gmail.com>
2026-04-20 01:30:59 +0700

    init project

--- RAG-based_Chatbot_For_Uni_Addmission-main/config.py
+++ RAG-based_Chatbot_For_Uni_Addmission-main/config.py
@@ -1,11 +1,13 @@
+import os
+from dotenv import load_dotenv
+load_dotenv("key.env")
 # --- Document and Paths ---
 DOC_PATH = "./input3.pdf"
 PERSIST_DIRECTORY = "./chroma_db_beta_rag" # Directory to store the Chroma vector database
-
 # --- Model Names ---
-MODEL_NAME = "nvidia/nemotron-3-super-120b-a12b:free" # Model gánh Text generation trên đường truyền OpenRouter
-OPENROUTER_API_KEY = "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" # BẮT BUỘC ĐIỀN API KEY TẠI ĐÂY
-EMBEDDING_MODEL = "bge-m3" # Vẫn giữ nguyên bge-m3 chạy bằng Ollama dưới máy em
+MODEL_NAME = "google/gemma-4-26b-a4b-it:free" # Model gánh Text generation trên đường truyền OpenRouter
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+EMBEDDING_MODEL = "bge-m3" # Vẫn giữ nguyên bge-m3 chạy bằng Ollama dưới máy
 
 # --- Vector Store ---
 VECTOR_STORE_NAME = "beta-rag"
